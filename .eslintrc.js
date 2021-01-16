b32950fb8ae9afdf5cda7d8e8b78d9170289316c
Max Holman <max@holmn.com>
2021-01-17 00:07:30 +0800

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: '@block65/eslint-config'
+  extends: '@block65/eslint-config',
+  parserOptions: {
+    project: ['./tsconfig.json', './__tests__/tsconfig.json'],
+  },
 };
